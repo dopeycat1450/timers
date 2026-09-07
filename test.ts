f571d4f90a459dda793ad8de6bfde1c6e9b13da4
dopeycat <florien@earp.co.uk>
2026-09-07 18:48:27 +0000

    made a test

--- test.ts
+++ test.ts
@@ -1,1 +1,15 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+timers.startTimer("test")
+
+basic.forever(() => {
+    basic.showNumber(timers.checkTimer("test"))
+    basic.pause(1000)
+})
+
+input.onButtonPressed(Button.A, function() {
+    timers.resetTimer("test")
+})
+
+input.onButtonPressed(Button.B, function() {
+    timers.stopTimer("test")
+})
